cc1b8cc18daf4944325b0504f7eb1bde00ea2386
Redwan <redwanahmed1@hotmail.co.uk>
2023-05-11 19:05:01 +0100

    Add css and added boiler template for book create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(title);
+  };
   return (
     <div>
-
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
